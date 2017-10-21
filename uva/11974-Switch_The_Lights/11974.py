1c1be7f6f008ddaf2a1855e53398d47b0bd38622
Ezequiel Barrios <ezequiel@barrios.com.ar>
2017-10-21 15:56:09 -0300

    problem uva/11974 Still TLE

--- uva/11974-Switch_The_Lights/11974.py
+++ uva/11974-Switch_The_Lights/11974.py
@@ -2,6 +2,50 @@
 # -*- coding: utf-8 -*-
 # https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3125
 
+def solve_for_switches(lamps_for_switch):
+    # initially all the states are unreachable
+    # except the initial state (all on) that is reachable with 0 switches
+    states_list = list()
+    states_count = [switch_count + 1 for i in range((2 ** lamp_count))]
+    defined_state = [False for i in range((2 ** lamp_count))]
+    states_count[initial_state] = 0
+    defined_state[initial_state] = True
+    states_list.append(initial_state)
+
+    for step in range(1, min(switch_count + 1, 16)):
+        # in this part, states will store the minimum number of switches needed to reach
+        # to the state represented by the "ith" key of the dict, using it as a binary mask
+        # eg., the state 5 will be 0101 = switch 0 on, switch 1 off, switch 2 on, switch 3 off
+
+        # now try all the switches combinations
+        new_states = list()
+        for switch in lamps_for_switch:
+            for s in states_list:
+                current_state = s ^ switch
+                # store the number of switches used to reach to this state, if it's less than the previous stored
+                if not defined_state[current_state]:
+                    # print("defining",current_state,switch)
+                    defined_state[current_state] = True
+                    new_states.append(current_state)
+        for state in new_states:  # define all the new discovered states
+            states_count[state] = step
+            states_list.append(state)
+
+        if defined_state[0]:
+            break
+
+    # ret_states_list = list()
+    # for state in states_list:
+    #    ret_states_list.append((states_count[state], state))
+    # ret_states_list.sort()
+
+    ret_states_list = set()
+    for state in states_list:
+        ret_states_list.add((states_count[state], state))
+
+    return ret_states_list, defined_state, states_count
+
+
 testcase_count = int(input())
 for testcase in range(testcase_count):
     lamp_count, switch_count = map(int, input().split())
@@ -11,41 +55,42 @@ for testcase in range(testcase_count):
         # eg., the switch "0 1 0 1" is stored as 5
         lamps_for_switch.append(int(input().replace(" ", ""), 2))
 
-
-    def make_states(lamps_for_switch):
-        # in this function, states will store the minimum number of switches needed to reach
-        # to the state represented by the "ith" position of the list, using it as a binary mask
-        # eg., the state 5 will be 0101 = switch 0 on, switch 1 off, switch 2 on, switch 3 off
-
-        # initially all the states are unreachable
-        # except the initial state (all on) that is reachable with 0 switches
-        states = [(switch_count + 1) for s in range(2 ** lamp_count)]
-        states[initial_state] = 0
-
-        # now try all the switches combinations
-        for i in range(2 ** len(lamps_for_switch)):
-            # we are using the integer "i" as a bit mask
-            switches_used = bin(i)[2:].zfill(len(lamps_for_switch))
-            current_state = initial_state
-            # start by 11...11, and apply the corresponding switches
-            for s in range(len(switches_used)):
-                if switches_used[s] == '1':
-                    current_state ^= lamps_for_switch[s]
-            # store the number of switches used to reach to this state, if it's less than the previous
-            states[current_state] = min(states[current_state], switches_used.count("1"))
-        return states
-
-
     initial_state = (2 ** lamp_count) - 1
-    states_A = make_states(lamps_for_switch[:int(switch_count / 2)])
-    states_B = make_states(lamps_for_switch[int(switch_count / 2):])
+    # print("SWITCHES_A_START")
+    switches_A = lamps_for_switch[:int(len(lamps_for_switch) / 2)]
+    states_list_A, defined_state_A, states_count_A = solve_for_switches(switches_A)
+    # print("SWITCHES_A_END")
+    # print("SWITCHES_B_START")
+    switches_B = lamps_for_switch[int(len(lamps_for_switch) / 2):]
+    states_list_B, defined_state_B, states_count_B = solve_for_switches(switches_B)
+    # print("SWITCHES_B_END")
 
-    min_switches_needed = switch_count + 1  # start with an impossible solution
-    for state in range(len(states_A)):
-        not_state = state ^ initial_state
-        min_switches_needed = min(min_switches_needed, states_A[state] + states_B[not_state])
-    for state in range(len(states_B)):
-        not_state = state ^ initial_state
-        min_switches_needed = min(min_switches_needed, states_B[state] + states_A[not_state])
+    win = False
+    win_count = 9999999
 
-    print("Case %s: %s" % (testcase + 1, min_switches_needed if min_switches_needed <= switch_count else "IMPOSSIBLE"))
+    for (a_count, a) in states_list_A:
+        if win:
+            break
+        b = initial_state ^ a
+        if defined_state_B[b]:
+            win = True
+            win_count = min(win_count, a_count + states_count_B[b])
+    for (b_count, b) in states_list_B:
+        if win:
+            break
+        a = initial_state ^ b
+        if defined_state_A[a]:
+            win = True
+            win_count = min(win_count, states_count_A[a] + b_count)
+
+    if defined_state_A[0]:
+        win = True
+        win_count = min(win_count, states_count_A[0])
+    if defined_state_B[0]:
+        win = True
+        win_count = min(win_count, states_count_B[0])
+
+    if win:
+        print("Case %s: %s" % (testcase + 1, win_count))
+    else:
+        print("Case %s: IMPOSSIBLE" % (testcase + 1))
