4e80591b0d90b6b903de4752117871195694885e
Ezequiel Barrios <ezequiel@barrios.com.ar>
2017-10-21 22:07:50 -0300

    problem uva/11974 accepted :)

--- uva/11974-Switch_The_Lights/11974.py
+++ uva/11974-Switch_The_Lights/11974.py
@@ -12,19 +12,20 @@ def solve_for_switches(lamps_for_switch):
     defined_state[initial_state] = True
     states_list.append(initial_state)
 
-    for step in range(1, min(switch_count + 1, 16)):
+    new_states = states_list
+    for step in range(1, switch_count + 1):
         # in this part, states will store the minimum number of switches needed to reach
         # to the state represented by the "ith" key of the dict, using it as a binary mask
         # eg., the state 5 will be 0101 = switch 0 on, switch 1 off, switch 2 on, switch 3 off
 
         # now try all the switches combinations
+        last_states = new_states
         new_states = list()
         for switch in lamps_for_switch:
-            for s in states_list:
+            for s in last_states:
                 current_state = s ^ switch
                 # store the number of switches used to reach to this state, if it's less than the previous stored
                 if not defined_state[current_state]:
-                    # print("defining",current_state,switch)
                     defined_state[current_state] = True
                     new_states.append(current_state)
         for state in new_states:  # define all the new discovered states
@@ -34,14 +35,10 @@ def solve_for_switches(lamps_for_switch):
         if defined_state[0]:
             break
 
-    # ret_states_list = list()
-    # for state in states_list:
-    #    ret_states_list.append((states_count[state], state))
-    # ret_states_list.sort()
-
-    ret_states_list = set()
+    ret_states_list = list()
     for state in states_list:
-        ret_states_list.add((states_count[state], state))
+        ret_states_list.append((states_count[state], state))
+    ret_states_list.sort()
 
     return ret_states_list, defined_state, states_count
 
@@ -56,33 +53,19 @@ for testcase in range(testcase_count):
         lamps_for_switch.append(int(input().replace(" ", ""), 2))
 
     initial_state = (2 ** lamp_count) - 1
-    # print("SWITCHES_A_START")
     switches_A = lamps_for_switch[:int(len(lamps_for_switch) / 2)]
     states_list_A, defined_state_A, states_count_A = solve_for_switches(switches_A)
-    # print("SWITCHES_A_END")
-    # print("SWITCHES_B_START")
     switches_B = lamps_for_switch[int(len(lamps_for_switch) / 2):]
     states_list_B, defined_state_B, states_count_B = solve_for_switches(switches_B)
-    # print("SWITCHES_B_END")
 
     win = False
     win_count = 9999999
 
     for (a_count, a) in states_list_A:
-        if win:
-            break
         b = initial_state ^ a
         if defined_state_B[b]:
             win = True
             win_count = min(win_count, a_count + states_count_B[b])
-    for (b_count, b) in states_list_B:
-        if win:
-            break
-        a = initial_state ^ b
-        if defined_state_A[a]:
-            win = True
-            win_count = min(win_count, states_count_A[a] + b_count)
-
     if defined_state_A[0]:
         win = True
         win_count = min(win_count, states_count_A[0])
