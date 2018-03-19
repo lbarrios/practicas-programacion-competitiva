f6f63cb8089e822f79600f2de9c80480399d6a98
Ezequiel Barrios <ezequiel@barrios.com.ar>
2018-03-19 14:51:50 -0300

    b-large to iterative

--- codejam/kickstart-2018/round-a/problem-b/b.py
+++ codejam/kickstart-2018/round-a/problem-b/b.py
@@ -2,23 +2,16 @@
 # -*- coding: utf-8 -*-
 # Codejam Kickstart 2018 - Round A - Problem B
 
-def expected_value(k):
-    if k not in _expected_value:
-        _expected_value[k] = 0
-        next_expected_value = expected_value(k - 1)
-        for (i, p) in items_with_p:
-            _expected_value[k] += max(i, next_expected_value) * p
-    return _expected_value[k]
-
-
 testcases = int(input())
 for t in range(testcases):
     N, K = list(map(int, input().split()))
     items = list(map(int, input().split()))
     items_with_p = [(i, items.count(i) / len(items)) for i in set(items)]
-    _expected_value = {0: sum(items) / len(items)}
 
-    for k in range(K):
-        expected_value(k)
+    expected_value = [sum(items) / len(items)] + [0] * K
+    for k in range(1, K + 1):
+        next_expected_value = expected_value[k - 1]
+        for (i, p) in items_with_p:
+            expected_value[k] += max(i, next_expected_value) * p
 
-    print("Case #%s: %.6f" % (t + 1, expected_value(K)))
+    print("Case #%s: %.6f" % (t + 1, expected_value[K]))
